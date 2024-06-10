0db07d87b23bb1031811e68fc162c89d60f09348
Sraucel <ryan.guo580@gmail.com>
2024-06-09 22:28:54 -0400

    folder organization and final button implementations

--- main.js
+++ main.js
@@ -42,3 +42,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     updateExperts(); // Initial population of experts dropdown
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
